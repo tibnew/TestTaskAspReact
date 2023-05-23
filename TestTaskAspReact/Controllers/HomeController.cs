ae6808fc0e3cfb162c38cbaf182aa9647446a6ef
tibnew <tibnew@tut.by>
2023-05-23 19:47:53 +0300

    Add User List with Name and Avatar

---
+++ TestTaskAspReact/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TestTaskAspReact.Data;
+
+namespace TestTaskAspReact.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+        public HomeController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            var userData = applicationDbContext.Users.Select(x => new {Name = x.UserName, ProfilePicture = x.ProfilePicture})
+                .ToList().Select(x =>
+            {
+                string? imgDataURL = null;
+                if (x.ProfilePicture != null)
+                {
+                    string imreBase64Data = Convert.ToBase64String(x.ProfilePicture);
+                    imgDataURL = string.Format("data:image/png;base64,{0}", imreBase64Data);
+                }
+                return new User { Name = x.Name, ProfilePictureUrl = imgDataURL };
+            }).ToArray();
+            return userData;
+        }
+    }
+
+    public class User
+    {
+        public string? Name { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+    }
+}
